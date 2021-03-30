9ab6e439a7651ab89380e0d5978ce32a465f7541
penghuanliang <1092860160@qq.com>
2021-03-30 11:18:16 +0800

    Detail page completed.

--- app/src/main/java/com/example/androiddevchallenge/ui/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.DemoDataProvider
 import com.example.androiddevchallenge.data.Puppy
